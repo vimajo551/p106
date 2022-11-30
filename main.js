d2f327ed05bbb5b1429d8259fff2712813f9bfc3
vimajo551 <104533881+vimajo551@users.noreply.github.com>
2022-11-30 11:12:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var dog =;
+var cat =;
+
 function startClassification(){
     navigator.mediaDevices.getUserMedia({audio:true})
     classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/0-ft2vPCL/model.json",modelReady)
@@ -7,4 +10,36 @@ function modelReady(){
 }
 function gotResults(error,results){
     console.log("gotResults")
+    if(error){
+        console.error(error)
+    }
+    else{
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = "Posso ouvir - "+results[0].label;
+        document.getElementById("result_count").innerHTML = "cachorro detectado - "+dog+ "gato detectado - "+cat;
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+
+
+        if(results[0].label=="cachorro"){
+            document.getElementById("r").src="download.png" = true;
+            document.getElementById("r").src="download (1).png" = false;
+            document.getElementById("r").src="download (2).png" = false;
+
+        }else if(results[0].label=="gato"){
+            document.getElementById("r").src="download.png";
+            document.getElementById("r").src="download (1).png";
+            document.getElementById("r").src="download (2).png";
+        }else{
+            document.getElementById("r").src="download.png";
+            document.getElementById("r").src="download (1).png";
+            document.getElementById("r").src="download (2).png";
+        }
+
+    }
+
 }
+
